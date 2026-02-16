ccadc863d8da1c6cf164fafdb07f66d85236d47a
Kaveewison <kaveewison_k@cmu.ac.th>
2026-02-17 01:40:35 +0700

    Promise Functions

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,9 @@ import {
   getEventByCategory,
   getEventById,
   addEvent,
-  getAllEventsWithOrganizer,
 } from "./services/EventService";
 import type Event from "./models/Event";
 
-import add from "./function";
 const app = express();
 app.use(express.json());
 const port = 3000;
@@ -17,29 +15,33 @@ app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-
 app.get("/events", (req, res) => {
   if (req.query.category) {
     const category = req.query.category as string;
-    const filteredEvents = getEventByCategory(category);
-    res.json(filteredEvents);
+    getEventByCategory(category).then((events: Event[]) => {
+      res.json(events);
+    });
   } else {
-    res.json(getAllEvents());
+    getAllEvents().then((events: Event[]) => {
+      res.send(events);
+    });
   }
 });
 
 app.get("/events/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const event = getEventById(id);
-  if (event) {
-    res.json(event);
-  } else {
-    res.status(404).send("Event not found");
-  }
+  getEventById(id).then((event: Event | undefined) => {
+    if (event) {
+      res.json(event);
+    } else {
+      res.status(404).send("Event not found");
+    }
+  });
 });
 
 app.post("/events", (req, res) => {
   const newEvent: Event = req.body;
-  addEvent(newEvent);
-  res.json(newEvent);
+  addEvent(newEvent).then((event: Event) => {
+    res.json(event);
+  });
 });
